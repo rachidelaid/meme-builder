2c96458d5f7951d11c4ef58aae63ee505da3e47f
rachid elaid <rachidelaid@gmail.com>
2022-03-14 19:44:16 +0100

    list memes from json

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+    <img :src="link" :alt="name" />
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['link', 'name'],
+};
+</script>
+
+<style scoped>
+.card img {
+  width: 100%;
+  aspect-ratio: 1;
+  border-radius: var(--radius);
+  border: 1px var(--white) solid;
+}
+
+.card:hover p {
+  bottom: 1px;
+  opacity: 1;
+}
+
+p {
+  position: absolute;
+  bottom: -20px;
+  opacity: 0;
+  transition: 0.2s all ease-in-out;
+  width: 100%;
+  padding: 10px;
+  background: var(--black);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="home">
+    <div class="list">
+      <Card
+        v-for="img in list"
+        :key="img.id"
+        :link="img.blank"
+        :name="img.name"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+import Card from '../components/Card.vue';
+
+import data from '../data.json';
+
+export default {
+  name: 'Home',
+  components: {
+    Card,
+  },
+  setup() {
+    const list = ref(data);
+
+    return { list };
+  },
+};
+</script>
+
+<style scoped>
+.list {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 10px;
+}
+</style>
